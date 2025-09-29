6f6bc14b4ec07d9b92ed9e2aba816f4c9131c787
julenberazaa <julenberaza.ehu@gmail.com>
2025-09-29 17:49:44 +0200

    anch más carrusel

--- app/layout.tsx
+++ app/layout.tsx
@@ -728,9 +728,9 @@ export default function RootLayout({
                   height: 76px !important;
                   min-height: 76px !important;
                   max-height: 76px !important;
-                  width: 138px !important; /* 20% wider than 115px */
-                  min-width: 138px !important;
-                  max-width: 138px !important;
+                  width: 166px !important; /* 20% wider than 138px */
+                  min-width: 166px !important;
+                  max-width: 166px !important;
                   display: block !important;
                   overflow: hidden !important;
                   position: relative !important;
